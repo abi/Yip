118951ed137d2d020b4a65ea282639c771128c10
Abimanyu Raja <abimanyuraja@gmail.com>
2009-07-26 22:43:12 +0800

    Forgot to include the UTF8 functions in the last commit.

--- components/yip.js
+++ components/yip.js
@@ -3,6 +3,75 @@ Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 const Ci = Components.interfaces;
 const Cc = Components.classes;
 
+
+// UTF8 functions used here under GNU GPL. Copyright (c) 2009 Ruben Verweij
+
+var utf8 = {
+ 
+	// public method for encoding
+	encode : function (string) {
+		if(typeof(string)!="undefined"&&string!=null){
+		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+ 
+		for (var n = 0; n < string.length; n++) {
+ 
+			var c = string.charCodeAt(n);
+ 
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			}
+			else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+			else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+ 
+		}
+ 
+		return utftext;
+		}
+		
+	},
+ 
+	// public method for decoding
+	decode : function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+ 
+		while ( i < utftext.length ) {
+ 
+			c = utftext.charCodeAt(i);
+ 
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+ 
+		}
+ 
+		return string;
+	}
+ 
+}
+
+
 function Yip() { }
 
 Yip.prototype = {
@@ -80,8 +149,8 @@ Yip.prototype = {
                    .createInstance(Ci.nsILocalFile);
         file.initWithPath("/usr/bin/notify-send");
 
-        var process = Components.classes["@mozilla.org/process/util;1"]
-            .createInstance(Components.interfaces.nsIProcess);
+        var process = Cc["@mozilla.org/process/util;1"]
+            .createInstance(Ci.nsIProcess);
         process.init(file);
         var args = [utf8.encode(title), utf8.encode(text), "-i", iconPath];
         process.run(false, args, args.length);
